6db2b6e0b48a69106c41600a211295e3477ae8ae
Johnny <jonathanbc2012@gmail.com>
2024-12-09 17:59:43 +0100

    feat: BudgetForm created

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,9 +1,45 @@
-
+import { useMemo, useState } from "react";
 
 function BudgetForm() {
+  const [budget, setBudget] = useState(0);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBudget(e.target.valueAsNumber);
+  };
+
+  const isValid = useMemo(()=>{
+    return isNaN(budget) || budget <=0
+  },[budget])
+
   return (
-    <div>BudgetForm</div>
-  )
+    <form className="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label
+          htmlFor="budget"
+          className="text-4xl text-blue-600 font-bold text-center"
+        >
+          Establecer Presupuesto
+        </label>
+        <input
+          id="budget"
+          type="number"
+          className="w-full bg-white border border-gray-200 p-2 "
+          placeholder="Establece tu presupuesto"
+          name="budget"
+          value={budget}
+          onChange={handleChange}
+          
+        />
+      </div>
+
+      <input
+        type="submit"
+        value="Establecer Presupuesto"
+        className="bg-blue-600 hover:bg-blue-700 w-full cursor-pointer p-2 text-white font-black uppercase disabled:opacity-30"
+        disabled={isValid}
+      />
+    </form>
+  );
 }
 
-export default BudgetForm
+export default BudgetForm;
