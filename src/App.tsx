26b217c9b416525872f6a5cdcc7e47cbff325cc9
Johnny <jonathanbc2012@gmail.com>
2024-12-09 18:39:12 +0100

    feat: custom hook useBudget created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import BudgetForm from "./components/BudgetForm";
 import Header from "./components/Header";
+import BudgetForm from "./components/BudgetForm";
+
 
 function App() {
+  
+
   return (
     <>
-      <Header/>
+      <Header />
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <BudgetForm/>
+        <BudgetForm />
       </div>
     </>
   );
--- src/context/BudgetContext.tsx
+++ src/context/BudgetContext.tsx
@@ -10,8 +10,11 @@ type BudgetProviderProps = {
     children: ReactNode
 }
 
+// El context es para generar el ámbito de aplicación de nuestro provider
 export const BudgetContext = createContext<BudgetContextProps>({} as BudgetContextProps) // para quitar el error, tb se puede pasar como argumento (null!)
 
+
+// El provider indica de donde vienen los datos y funciones
 export const BudgetProvider = ({children}: BudgetProviderProps) =>{
 
     const [state, dispatch] = useReducer(budgetReducer, initialState)
