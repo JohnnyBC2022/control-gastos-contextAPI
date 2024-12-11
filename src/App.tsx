5447c4cf3c2e405a022d89216f4426706de5d056
Johnny <jonathanbc2012@gmail.com>
2024-12-11 13:39:50 +0100

    feat: headlesUI and heroIcons libraries installed and ExpenseModal componente created

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,27 @@ import Header from "./components/Header";
 import BudgetForm from "./components/BudgetForm";
 import { useBudget } from "./hooks/useBudget";
 import BudgetTracker from "./components/BudgetTracker";
+import ExpenseModal from "./components/ExpenseModal";
 
 function App() {
-  const {state} = useBudget()
+  const { state } = useBudget();
 
-  const isValidBudget = useMemo(()=> state.budget > 0, [state.budget])
+  const isValidBudget = useMemo(() => state.budget > 0, [state.budget]);
   return (
     <>
       <Header />
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        {isValidBudget ? <BudgetTracker/> :<BudgetForm />}
+        {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
       </div>
+
+      {isValidBudget && (
+        <main className="max-w-3xl mx-auto py-10">
+
+          
+          <ExpenseModal />
+        </main>
+      )}
     </>
   );
 }
