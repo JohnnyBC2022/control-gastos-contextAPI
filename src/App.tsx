397e22dceedc89670ebda274dba8217b609ba94a
Johnny <jonathanbc2012@gmail.com>
2024-12-11 13:29:29 +0100

    feat: AmountDisplay and BudgetTracker components created, helper formatCurrency function created

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+import { useMemo } from "react";
 import Header from "./components/Header";
 import BudgetForm from "./components/BudgetForm";
-
+import { useBudget } from "./hooks/useBudget";
+import BudgetTracker from "./components/BudgetTracker";
 
 function App() {
-  
+  const {state} = useBudget()
 
+  const isValidBudget = useMemo(()=> state.budget > 0, [state.budget])
   return (
     <>
       <Header />
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <BudgetForm />
+        {isValidBudget ? <BudgetTracker/> :<BudgetForm />}
       </div>
     </>
   );
---
+++ src/helpers/index.ts
@@ -0,0 +1,3 @@
+export function formatCurrency(amount: number) {
+    return new Intl.NumberFormat('es-Es', { style: 'currency', currency: 'EUR' }).format(amount)
+}
