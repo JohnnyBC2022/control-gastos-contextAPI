7a42d799e4059e68ef189122cd37aa97f6d264d1
Johnny <jonathanbc2012@gmail.com>
2024-12-12 19:14:55 +0100

    feat: ExpenseDetail and ExpenseList components created

---
+++ src/components/ExpenseDetail.tsx
@@ -0,0 +1,11 @@
+import { Expense } from "../types";
+
+type ExpenseDetailProps = {
+  expense: Expense;
+};
+
+function ExpenseDetail({ expense }: ExpenseDetailProps) {
+  return <div>ExpenseDetail</div>;
+}
+
+export default ExpenseDetail;
