93171db4bea796163a0b74753500dd9cb4a73bea
Johnny <jonathanbc2012@gmail.com>
2024-12-13 11:04:43 +0100

    feat: AmountDisplay and ExpenseDetail components modified

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -10,7 +10,7 @@ function BudgetTracker() {
       <div className="flex flex-col justify-center items-center gap-8">
         <button
           type="button"
-          className="bg-pink-600 w-full p-2 text-white uppercase font-bold"
+          className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
         >
           Resetear App
         </button>
--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,11 +1,42 @@
+import { useMemo } from "react";
+import { formatDate } from "../helpers";
 import { Expense } from "../types";
+import AmountDisplay from "./AmountDisplay";
+import { categories } from "../data/categories";
 
 type ExpenseDetailProps = {
   expense: Expense;
 };
 
 function ExpenseDetail({ expense }: ExpenseDetailProps) {
-  return <div>ExpenseDetail</div>;
+  const categoryInfo = useMemo(
+    () => categories.filter((cat) => cat.id === expense.category)[0],
+    [expense]
+  );
+
+  return (
+    <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center">
+      <div>
+        <img
+        src={`/icono_${categoryInfo.icon}.svg`}
+        alt="icono gasto"
+        className="w-20"
+        />
+      </div>
+
+      <div className="flex-1 space-y-2">
+        <p className="text-sm font-bold uppercase text-slate-500">
+          {categoryInfo.name}
+        </p>
+        <p>{expense.expenseName}</p>
+        <p className="text-slate-600 text-sm">
+          {formatDate(expense.date!.toString())}
+        </p>{" "}
+        {/* El signo de exclamación es que le estamos garantizando que ese valor va a existir */}
+      </div>
+      <AmountDisplay amount={expense.amount} />
+    </div>
+  );
 }
 
 export default ExpenseDetail;
